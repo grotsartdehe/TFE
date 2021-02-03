57977a0c42b919d3203a5ae8ae4fd1e1bcfdcd13
kdesousa97 <desousakevin97@gmail.com>
2021-02-03 21:19:22 +0100

    Load_Data.py
    Proposition d'extraction des données

--- simu radar/main.py
+++ simu radar/main.py
@@ -2,19 +2,24 @@ import pandas as pd
 import re
 from RadarGen import *
 from Search import *
-text_file = open("01_gt.txt", "r")
- 
-#read whole file to a string
-data = text_file.read()
- 
-#close file
-text_file.close()
+from io import StringIO
 
-"""
-doc_string = re.split('[a-z,:,;,=,-,@]+', data, flags = re.IGNORECASE)
-print(doc_string)
-"""
+string = 'prototype.txt'
 
+data = pd.read_csv(string)
+data.head()
+
+
+
+
+StringData = StringIO(data.iat[2,6]) 
+step = 'Vehicul.txt'
+col = ['ID', 'Class', 'Xpos', 'Ypos','Zpos','X2D','Y2D','v','xdir','ydir','zdir','Xbox','Ybox','Zbox','Xext',\
+       'Yext','Zext','XYZ@X','XYZ@Y','XYZ@Z','XY-Z@X','XY-Z@Y','XY-Z@Z','X-YZ@X','X-YZ@Y','X-YZ@Z','X-Y-Z@X','X-Y-Z@Y','X-Y-Z@Z',\
+      '-XYZ@X','-XYZ@Y','-XYZ@Z','-XY-Z@X','-XY-Z@Y','-XY-Z@Z','-X-YZ@X','-X-YZ@Y','-X-YZ@Z','-X-Y-Z@X','-X-Y-Z@Y','-X-Y-Z@Z',\
+      'X2DC', 'Y2DC','XY-Z2@X','XY-Z2@Y','X-YZ2@X','X-YZ2@Y','X-Y-Z2@X','X-Y-Z2@Y','-XYZ2@X','-XYZ2@Y','-XY-Z2@X','-XY-Z2@Y','-X-YZ2@X','-X-YZ2@Y','XYZ2@X','-X-Y-Z2@Y']
+rep = pd.read_csv(StringData,sep=';',names =col ,index_col=False, lineterminator = '\\') 
+print(rep.shape)
 
 """
 names = ['Frame', 'T:', 'Time', 'ff', 'Pos', 'ffg', 'Position' , 'Detected', 'Pawns' ,'hfhe' ,'Class', '[List of classes']
@@ -23,8 +28,9 @@ doc = pd.read_csv(data,delimiter=names )
 doc.info()
 doc.shape()
 """
-
+"""
 Z1,Z2 = RadarGen(0,[30,5],[35,5],[np.pi/3,6],[np.pi/7,np.pi/8])
 res = Searchdv(Z1,1)
 resangle= Searchangle(Z2,1)
 
+"""
